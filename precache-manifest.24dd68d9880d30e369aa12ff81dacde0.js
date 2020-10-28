4806c638b8c185d6d2c662d0f2cf04a1d6bea706
RajibMiah <ahmedrajib942@gmail.com>
2020-10-28 05:07:24 -0700

    Updates

---
+++ precache-manifest.24dd68d9880d30e369aa12ff81dacde0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f7207e01b4d531959d7965836ec623",
+    "url": "/HRAdminPanel_react_customized_mui/index.html"
+  },
+  {
+    "revision": "c5e227268f82661f84c0",
+    "url": "/HRAdminPanel_react_customized_mui/static/css/main.b2f11e85.chunk.css"
+  },
+  {
+    "revision": "3f64b8c6131009252edc",
+    "url": "/HRAdminPanel_react_customized_mui/static/js/2.b64b1c72.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/HRAdminPanel_react_customized_mui/static/js/2.b64b1c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e227268f82661f84c0",
+    "url": "/HRAdminPanel_react_customized_mui/static/js/main.92abf883.chunk.js"
+  },
+  {
+    "revision": "f046e6a3c2ffeb12f550",
+    "url": "/HRAdminPanel_react_customized_mui/static/js/runtime-main.b39b185d.js"
+  }
+]);
